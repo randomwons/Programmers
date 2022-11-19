7ffe3e4f8b04d224c85d27b766286b2543117f26
jinwon <randomwons@gmail.com>
2022-11-20 08:00:34 +0900

    7의 개수

---
+++ Lv0/7의 개수.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array){
+    int answer = 0;
+    for(auto& v : array){
+        while(v != 0){
+            if (v%10 == 7) answer++;
+            v /= 10;
+        }
+    }
+    return answer;
+}
